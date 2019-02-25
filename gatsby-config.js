3a5fea7a86436f9322b4d35da873bab8cc5fa816
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-02-25 20:22:12 +0200

    remove unnecessary files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Vulekamali Create`,
+    description: `Vulekamali Hackathon page. Build websites, mobile apps and other tools using the data and APIs behind vulekamali`,
+    author: ``,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,8 +25,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `minimal-ui`,// This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
