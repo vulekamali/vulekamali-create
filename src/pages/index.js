f2ad5b3f8c1fc0d76da47ce33eb2ef6dd68cf835
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-02-26 21:00:31 +0200

    refactor main event component card

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Header from '../components/header';
 import NavigationsCopy from '../data/navigationApi.json';
 import Hero from '../components/hero';
 import Footer from '../components/footer';
+import MainEventCard from '../components/mainEvent';
 
 const IndexPage = () => (
   <React.Fragment>
     <Header links={NavigationsCopy} />
     <Hero />
+    <MainEventCard />
     <Footer />
   </React.Fragment>
 );
