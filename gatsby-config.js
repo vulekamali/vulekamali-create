89b885c61338aa36d07306cb4704c13213934221
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-02-26 10:56:35 +0200

    update config to fix eslint errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Vulekamali Create`,
-    description: `Vulekamali Hackathon page. Build websites, mobile apps and other tools using the data and APIs behind vulekamali`,
-    author: ``,
+    title: 'Vulekamali Create',
+    description: 'Vulekamali Hackathon page. Build websites, mobile apps and other tools using the data and APIs behind vulekamali',
+    author: '',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-material-ui`,
-    `gatsby-plugin-styled-components`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-material-ui',
+    'gatsby-plugin-styled-components',
   ],
 }
