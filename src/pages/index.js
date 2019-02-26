9cf21651e4e9ea7584b80e2e4e8d6b5d053b99da
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-02-26 11:10:36 +0200

    add google font Lato to project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../components/header';
+import AppWrapper from '../components/AppWrapper';
 
 const IndexPage = () => (
-  <React.Fragment>
+  <AppWrapper>
     <Header />
-  </React.Fragment>
+  </AppWrapper>
 );
 
 export default IndexPage;
