20ecd38ce81bb1efb4bc28a1eeaa8d076fa093bc
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-02-26 17:48:11 +0200

    add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../components/header';
 import NavigationsCopy from '../data/navigationApi.json';
 import Hero from '../components/hero';
+import Footer from '../components/footer';
 
 const IndexPage = () => (
   <React.Fragment>
     <Header links={NavigationsCopy} />
     <Hero />
+    <Footer />
   </React.Fragment>
 );
 
