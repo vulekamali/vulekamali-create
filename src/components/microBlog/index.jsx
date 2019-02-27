f2035d644003c143b0c52b29b68adbc2417cfe1a
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-02-27 18:36:42 +0200

    initial attempt at embedding microblog

---
+++ src/components/microBlog/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MicroBlog = () => (
+    <div>
+        <div id="LB24_LIVE_CONTENT" data-eid="2200802748257424172" />
+    </div>
+);
+
+export default MicroBlog;
