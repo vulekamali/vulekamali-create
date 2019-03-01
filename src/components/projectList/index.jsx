9c68a4a32a50a95f97928c6d12e4c99d8272b7ef
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-03-01 16:25:49 +0200

    add background pattern when no image provided

--- src/components/projectList/index.jsx
+++ src/components/projectList/index.jsx
@@ -76,9 +76,9 @@ const ImageContainer = styled.div`
 `;
 
 const ImageStyle = styled.div`
-  background-image: ${({ cover }) => (cover ? `url('${cover}')` : 'none')};
+  background-image: ${({ cover }) => (cover ? `url('${cover}')` : `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='199' viewBox='0 0 100 199'%3E%3Cg fill='%239C92AC' fill-opacity='0.4'%3E%3Cpath d='M0 199V0h1v1.99L100 199h-1.12L1 4.22V199H0zM100 2h-.12l-1-2H100v2z'%3E%3C/path%3E%3C/g%3E%3C/svg%3E")`)};
   background-color: rgba(0, 0, 0, 0.12);
-  background-size: cover;
+  background-size: ${({ cover }) => (cover ? `cover` : `none`)};
   background-position: center;
   width: 100%;
   height: 100%;
