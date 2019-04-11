78af08e7313fbdf6feaa8adf3bbc8f05d6a45885
Mish <mishuct26@gmail.com>
2019-04-11 19:15:10 +0200

    sets dangerouslySetInnerHTML object

--- src/components/microBlog/index.jsx
+++ src/components/microBlog/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Typography } from '@material-ui/core';
 import ContainerLayout from '../containerLayout';
 import GlobalStyle from './GlobalStyle';
+import hardCodedHtml from './hardCodedHtml';
 
 const Wrapper = styled.div`
   display: flex;
@@ -16,30 +16,11 @@ const Wrapper = styled.div`
   }
 `;
 
-const MicroBlogEmbed = styled.div`
-  margin: 0 auto;
-`;
-
-const Title = styled(Typography)`
-  font-family: Lato;
-  font-style: normal;
-  font-weight: 900;
-  line-height: 40px;
-  font-size: 25px;
-  letter-spacing: 0.01em;
-  color: #243858;
-
-  @media screen and (min-width: 650px) {
-    font-size: 35px;
-  }
-`;
-
 const MicroBlog = () => (
   <Wrapper>
     <GlobalStyle />
     <ContainerLayout>
-      <Title>Timeline</Title>
-      <MicroBlogEmbed id="LB24_LIVE_CONTENT" data-eid="2200802748257424172" />
+      <div dangerouslySetInnerHTML={{ __html: hardCodedHtml }} />
     </ContainerLayout>
   </Wrapper>
 );
