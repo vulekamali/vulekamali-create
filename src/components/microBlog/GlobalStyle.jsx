a8183bbd257d8d68d987d7547eed2491e21eccb4
JD Bothma <jbothma@gmail.com>
2019-05-08 12:04:50 +0200

    Revert "changes id names"
    This reverts commit 260bed23d5c2f9f413037236efaa902a2111f36d.

--- src/components/microBlog/GlobalStyle.jsx
+++ src/components/microBlog/GlobalStyle.jsx
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  .vulekamali-default-list-item-title {
+  .lb24-default-list-item-title {
     color: #243858 !important;
     font-size: 12px !important;
     font-family: Lato;
@@ -11,12 +11,12 @@ const GlobalStyle = createGlobalStyle`
   }
 
   @media screen and (min-width: 650px) {
-    .vulekamali-default-list-item-title {
+    .lb24-default-list-item-title {
       font-size: 14px !important;
     }
   }
 
-  .vulekamali-default-list-item {
+  .lb24-default-list-item {
     overflow: hidden;
     box-shadow: 0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12);
     background-color: #fff;
@@ -25,7 +25,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 20px !important;
   }
 
-  .vulekamali-default-list-item-date {
+  .lb24-default-list-item-date {
     color: #8091a5 !important;
     font-size: 12px !important;
     font-family: Lato;
@@ -35,12 +35,12 @@ const GlobalStyle = createGlobalStyle`
   }
 
   @media screen and (min-width: 650px) {
-    .vulekamali-default-list-item-date {
+    .lb24-default-list-item-date {
       font-size: 14px !important;
     }
   }
 
-  .vulekamali-component-content {
+  .lb24-component-content {
     font-size: 14px !important;
     font-family: Lato;
     font-weight: 400;
@@ -49,13 +49,13 @@ const GlobalStyle = createGlobalStyle`
     color: #42526 !important;
   }
 
-  #vulekamali [contenteditable="false"] {
+  #LB24 [contenteditable="false"] {
     display: flex;
     justify-content: center;
     width: 100%;
   }
 
-  .vulekamali-vote {
+  .lb24-vote {
     display: static !important;
   }
 `;
