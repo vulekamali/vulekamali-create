8fb774c2c29a1f6f631065066a942bb120548890
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-06-06 12:10:35 +0200

    add title in landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../components/header';
+import Hero from '../components/hero';
 
 const IndexPage = () => (
   <React.Fragment>
     <Header />
+    <Hero title="Create with us!" />
   </React.Fragment>
 );
 
