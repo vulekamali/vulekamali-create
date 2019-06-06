eb40746aaf1e4f44bf05a1a5bef43b8bc456ae01
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-06-06 11:52:39 +0200

    add event page and new index page

---
+++ src/pages/event.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Header from '../components/header';
+import Hero from '../components/hero';
+import Footer from '../components/footer';
+import MainEventCard from '../components/mainEvent';
+import ProjectList from '../components/projectList';
+import MicroBlog from '../components/microBlog';
+
+const EventPage = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Better Budget Dataquest Northern Cape</title>
+      <meta name="twitter:card" content="summary" />
+      <meta property="og:title" content="Better Budget Dataquest Northern Cape" />
+      <meta property="og:description" content="Explore the spending data, and come up with findings or indications under one category of gender, inequality or the environment. Contextualize the spending with other data available to you." />
+      <meta name="description" content="Explore the spending data, and come up with findings or indications under one category of gender, inequality or the environment. Contextualize the spending with other data available to you." />
+    </Helmet>
+    <Header />
+    <Hero />
+    <MainEventCard />
+    <ProjectList />
+    <MicroBlog />
+    <Footer />
+  </React.Fragment>
+);
+
+export default EventPage;
