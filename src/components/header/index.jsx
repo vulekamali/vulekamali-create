96c8f5ba74b34f5714496048cd7eaa2cf0903f7c
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-06-11 09:56:25 +0200

    fix navbar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,69 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import ContainerLayout from '../containerLayout';
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 68px;
-  background-color: #21292E;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const NavItemsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ListContainer = styled.ul`
-  display: flex;
-  justify-content: space-between;
-  padding: 0;
-`;
-
-const List = styled.li`
-  list-style: none;
-  text-align: center;
-  padding-right: 32px;
-`;
-
-const LeftItemsList = styled.li`
-  list-style: none;
-  text-align: center;
-  padding-right: 32px;
-`;
-
-const RightItemsList = styled.li`
-  list-style: none;
-  text-align: center;
-  padding-left: 32px;
-`;
-
-const LinkStyle = styled.a`
-  text-decoration: none;
-  color: rgba(255, 255, 255, 0.8);
-  text-transform: Uppercase;
-  font-family: Lato;
-  font-weight: 700;
-  line-height: normal;
-  font-size: 12px;
-
-  &:hover {
-    color: #fff;
-  }
-
-  &:focus {
-    color: #8ECA62;
-    font-weight: 400;
-  }
-
-  @media screen and (min-width: 450px) {
-    font-weight: 900;
-    font-size: 14px;
-  }
-`;
+import {
+  Wrapper,
+  NavItemsContainer,
+  ListContainer,
+  LeftItemsList,
+  LinkStyle,
+  List,
+  RightItemsList
+} from './styled';
 
 const Header = () => (
   <Wrapper>
